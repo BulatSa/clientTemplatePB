b0529c53c49515c2bda394d3e3a2cdff4372a7f8
Bulat <sadikovba@gmail.com>
2018-02-01 10:49:38 +0300

    Initial

---
+++ black.php
@@ -0,0 +1,32 @@
+﻿<?php include('header.php'); ?>
+
+<section class="header-sec header-sec--black">
+	<div class="header container">
+		<div class="header__title">
+			<span>Landing Page </span>
+			<p>стоматологическая клиники Top Smile</p>
+		</div>
+	</div>
+</section>
+
+
+<section class="work-sec work-sec--black">
+	<div class="work container">
+		<div class="work-img">
+			<img src="img/work-example.jpg" alt="">
+		</div>
+	</div>
+</section>
+
+
+<section class="footer-sec footer-sec--black">
+	<div class="footer container">
+		<div class="footer__title">
+			<p>design with reason. create with passion.<sup>*</sup></p>
+			<span>думай головой. создавай сердцем</span>
+		</div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<?php include('header.php'); ?>
+
+<section class="header-sec">
+	<div class="header container">
+		<div class="header__title">
+			<span>Landing Page </span>
+			<p>стоматологическая клиники Top Smile</p>
+		</div>
+	</div>
+</section>
+
+
+<section class="work-sec">
+	<div class="work container">
+		<div class="work-img">
+			<img src="img/work-example.jpg" alt="">
+		</div>
+	</div>
+</section>
+
+
+<section class="footer-sec">
+	<div class="footer container">
+		<div class="footer__title">
+			<p>design with reason. create with passion.<sup>*</sup></p>
+			<span>думай головой. создавай сердцем</span>
+		</div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
